dffa96dfa3e533fdb93149f3581792a88711fb5c
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-03-11 06:31:52 +0600

    update ui

---
+++ index.js
@@ -0,0 +1,9 @@
+function saveInputFieldText(id){
+    const inputField = document.getElementById(`${id}`);
+    const inputFieldValue =inputField.value;
+    console.log('nahid');
+
+
+
+    // document.getElementById(id).value='';
+}
